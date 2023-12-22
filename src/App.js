59c5c69369a155353ab70a4a5a871114c49f2f8e
madhumitaamaharaj <madhumitaamaharaj7@gmail.com>
2023-12-22 16:36:22 +0530

    Feat:Sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Head from './components/Header/Head';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
       <Head/>
+      <Sidebar/>
     </div>
   );
 }
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react';
+import SidebarStyles from './Sidebar.module.css';
+import { RiStickyNoteFill } from "react-icons/ri";
+import { BsBell,BsFillTrashFill} from 'react-icons/bs';
+import { MdEdit, MdOutlineArchive } from "react-icons/md";
+
 
 const Sidebar = () => {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
+
+ 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  if (!isMenuOpen) return null;
+
   return (
-    <div>
+    <div className={SidebarStyles.container}>
+      <ul className={SidebarStyles.listItem}>
+        <li className={SidebarStyles.listItem}>
+          <RiStickyNoteFill className={SidebarStyles.icon} />
+          <span className={SidebarStyles.listItemText}>Notes</span>
+        </li>
+        <li className={SidebarStyles.listItem}>
+          <BsBell className={SidebarStyles.icon} />
+          <span className={SidebarStyles.listItemText}>Reminder</span>
+        </li>
+        <li className={SidebarStyles.listItem}>
+          <MdEdit className={SidebarStyles.icon} />
+          <span className={SidebarStyles.listItemText}>Edit Labels</span>
+        </li>
+        <li className={SidebarStyles.listItem}>
+          <MdOutlineArchive className={SidebarStyles.icon} />
+          <span className={SidebarStyles.listItemText}>Archive</span>
+          <li className={SidebarStyles.listItem}>
+          <BsFillTrashFill className={SidebarStyles.icon} />
+          <span className={SidebarStyles.listItemText}>Trash</span>
+        </li>
+        </li>
+      </ul>
       
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
